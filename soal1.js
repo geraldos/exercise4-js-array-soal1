e34466c88ebdeca0981a895e073157830d07ee44
geraldos <sepdwijaya18@gmail.com>
2020-06-12 15:30:27 +0700

    first commit

---
+++ soal1.js
@@ -0,0 +1,12 @@
+let jobsIT = ["FrontEnd Developer", "BackEnd Developer", "Security Engineering", "Software Engineering", "UI/UX"]
+
+jobsIT.length;
+
+console.log(jobsIT.length)
+console.log(jobsIT)
+
+document.getElementById("list").innerHTML = (jobsIT[0])
+document.getElementById("list2").innerHTML = (jobsIT[1])
+document.getElementById("list3").innerHTML = (jobsIT[2])
+document.getElementById("list4").innerHTML = (jobsIT[3])
+document.getElementById("list5").innerHTML = (jobsIT[4])
